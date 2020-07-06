9fd20386f46ccb3a2810549f7d9740d853a93349
David Laribee <laribee@gmail.com>
2020-07-06 16:02:42 -0400

    Configure empty project w/ Junit 5 and Mockito.

---
+++ src/main/java/playground/Library.java
@@ -0,0 +1,7 @@
+package playground;
+
+public class Library {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
---
+++ src/test/java/playground/LibraryTest.java
@@ -0,0 +1,13 @@
+package playground;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class LibraryTest {
+    @Test @DisplayName("Start here.") void someLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+    }
+}
